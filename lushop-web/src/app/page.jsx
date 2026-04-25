37d3bc2aaab1cbc3a856bd7befdec7b468705901
lushopbout-oficial <lushopbout@gmail.com>
2026-04-25 14:03:55 -0500

    Update page.jsx
    V1.2

--- lushop-web/src/app/page.jsx
+++ lushop-web/src/app/page.jsx
@@ -3,6 +3,7 @@ import CategoryGrid from '@/components/CategoryGrid';
 import ProductGrid from '@/components/ProductGrid';
 import DarkBanner from '@/components/DarkBanner';
 import TrustBar from '@/components/TrustBar';
+import Footer from '@/components/Footer'; // 1. Importamos el Footer
 
 export default function Home({ searchParams }) {
   const categoria = searchParams?.categoria || null;
@@ -14,6 +15,7 @@ export default function Home({ searchParams }) {
       <ProductGrid initialCategory={categoria} />
       <DarkBanner />
       <TrustBar />
+      <Footer /> {/* 2. Lo colocamos al final para cerrar la página */}
     </>
   );
 }
